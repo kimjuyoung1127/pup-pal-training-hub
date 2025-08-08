32141e7b74855ef252429920b5174004951de01e
김주영 <gmdqn2tp@email.com>
2025-08-08 21:07:21 +0900

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import sitemap from 'vite-plugin-sitemap';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,37 @@ export default defineConfig({
       exclude: ['/success', '/fail', '/*.html'],
       // robots.txt 자동 생성 비활성화
       generateRobotsTxt: false,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Pet-Life',
+        short_name: 'Pet-Life',
+        description: 'AI 훈련 코치를 담은 지능형 펫 플랫폼',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
